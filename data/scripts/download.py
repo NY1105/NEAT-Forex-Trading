c3661f17168b7b036792ce2d6149a5e24503ef93
NY1105 <yanwaiyin1105@gmail.com>
2022-07-05 17:27:44 +0800

    Update download.py

--- data/scripts/download.py
+++ data/scripts/download.py
@@ -1,23 +1,27 @@
 import os
+import pathlib
 import pandas as pd
 import yfinance as yf
 from datetime import timedelta, datetime
 
+################################################################################################################
 symbol = 'USDJPY=X'
 interval = '1m'
-period = '5d'
-END = '2022-06-24'
-# END = datetime.today()
+start = datetime.today() - timedelta(days=29) # start = '2022-06-20'
+end = start + timedelta(days=5) # end = datetime(int(start[:4]),int(start[5:7]),int(start[8:])) + timedelta(days=5)
+################################################################################################################
 
+path = pathlib.PurePath(pathlib.Path('download.py').parent.absolute(), 'data', 'train')
+pathlib.Path(path).mkdir(parents=True, exist_ok=True)
 
 try:
     print(symbol)
-    data = yf.download(symbol, end=END, interval=interval,period=period)
+    data = yf.download(symbol, start=start, end=end, interval=interval)
     symbol = symbol[0:6]
     file = symbol + ".csv"
     data = data.dropna()
     data.to_csv('data/train/'+symbol+".csv")
-except:
+except Exception as e:
+    print(e)
     print("Unable to load data for {}".format(symbol))
-
 
