c7defeccfb8c5f4f371df0bfa0269cfa1d1ab1b3
LamTsunHay <otinilam@gmail.com>
2022-07-06 17:21:03 +0800

    fix bug

--- indicators.py
+++ indicators.py
@@ -1,22 +1,21 @@
+from textwrap import indent
 import pandas as pd
 
 df = pd.read_csv ('EURUSD.csv')
+df['FastSMA'] = df['Close'].rolling(7, min_periods=7).sum().fillna(df['Close'])
+df['SlowSMA'] = df['Close'].rolling(20, min_periods=20).sum().fillna(df['Close'])
 
-class Indicators:
+class Indicators:        
 
-    def __init__(self, time):
-        self.time = time
-        df['SMA'] = df['Close'].rolling(20, min_periods=20).fillna(df['Close'])
-
-    def get_volume(self):
-        return df['Volume(Millions)'].iloc[self.time]
+    def get_volume(self, time):
+        return df['Volume(Millions)'].iloc[time]
     
-    def get_close_price(self):
-        return df['Close'].iloc[self.time]
+    def get_close_price(self, time):
+        return df['Close'].iloc[time]
 
     def get_df(self):
         return df    
 
-    def get_SMA(self):
-        return df['SMA'].iloc[self.time]
+    def get_SMA_diff(self, time):
+        return df['FastSMA'].iloc[time]-df['SlowSMA'].iloc[time]
 
