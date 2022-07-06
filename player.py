cf9c93cf8550fd8205e47e8dc65cab166293b8c2
leoleo1222 <rx4498@gmail.com>
2022-07-06 16:19:01 +0800

    Player Can Trade

--- player.py
+++ player.py
@@ -1,24 +1,31 @@
 class Trade:
 
-    def open_order(cash_total, action):  # open order - - buy/sell
-        return cash_total
+    def open_order(cash_total, action, record, index, df):  # open order - - buy/sell
+        if record[action][0] != 0:
+            record[action][1] = df['Close'].iloc[index]
+            return
+        record[action][0] = df['Close'].iloc[index]
 
-    def close_order(cash_total, action):  # calculating profit - - close
+    def close_order(cash_total, action, record, index, df):  # calculating profit - - close
+        if record[action][1] == 0:
+            return cash_total
+        cash_total += cash_total * (record[action][1] - record[action][0])
         return cash_total
 
 
 class Player:
 
-    def __init__(self, name: str, action: str):
-        self.name = name              # name         :  the name of the player
-        self.action = action          # action       :  buy/sell/close/no action
-        self.__cash_total = 0
+    def __init__(self, name, df):
+        self.name = name                               # name       :  the name of the player
+        self.record = {'buy': [0, 0], 'sell': [0, 0]}  # record     :  store the row of the trading
+        self.__cash_total = 1000                       # cash total :  the initial cash amount of the player
+        self.df = df                                   # df         :  it is the dataframe
 
-    def get__cash_total(self):  # the cash flow of the player
+    def get_cash_total(self):  # the cash flow of the player
         return self.__cash_total
 
-    def order_execute(self):  # the execution call for the trade
-        if self.action == 'buy' or 'sell':
-            Trade.open_order(self.__cash_total, self.action)
-        else:
-            Trade.close_order(self.__cash_total, self.action)
+    def order_execute(self, action, index):  # the execution call for the trade
+        if self.action in ('buy', 'sell'):
+            Trade.open_order(self.__cash_total, action, self.record, index, self.df)
+        elif self.action == 'close':
+            self.__cash_total = Trade.close_order(self.__cash_total, action, self.record, index, self.df)
