9c178f7a06bbbe0bb9858c4180d84b81a5a7b602
leoleo1222 <rx4498@gmail.com>
2022-07-07 17:08:01 +0800

    environment updated

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import player
 import indicators
+from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.backends.backend_agg as agg
+import pygame
 
 
 def main():
-    p1 = player.Player("Leo", indicators.df)
-    p1.print_cash()
-    p1.sell(0)
-    p1.close(1)
-    p1.print_cash()
+    draw()
+
+
+def draw():  # this plot the drawing of the period
+    fig = plt.figure(figsize=(13, 5))
+    price = indicators.df['Close']
+    ax = fig.add_subplot()
+    ax.set_ylabel('Price')
+    ax.set_xlabel('Number Of Days')
+    plt.plot(price)
+    plt.show()
 
 
 if __name__ == '__main__':
