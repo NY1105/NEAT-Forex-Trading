f42d624c27a39da8b9c258bc42dd5b5fcf67b2c4
NY1105 <yanwaiyin1105@gmail.com>
2022-07-08 23:52:27 +0800

    New Data Fetch Method

--- data/scripts/download.py
+++
@@ -1,5 +0,0 @@
-import os
-
-symbol = 'USDJPY'
-date = '2022-06-22'
-os.system('duka '+ symbol +' -d '+ date +' -c M1 -f data/train --header')
--- indicators.py
+++ indicators.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-
+SYMBOL = 'USDJPY'
 class Indicators:
     def __init__(self):
-        self.df = pd.read_csv('EURUSD.csv')
+        self.df = pd.read_csv(f'data/csv/{SYMBOL}.csv')
         self.df['FastSMA'] = self.df['Close'].rolling(50, min_periods=50).mean().fillna(self.df['Close'])
         self.df['SlowSMA'] = self.df['Close'].rolling(100, min_periods=100).mean().fillna(self.df['Close'])
         self.df.dropna(inplace=True)
@@ -13,7 +13,7 @@ class Indicators:
         return self.df
 
     def get_volume(self, index):
-        return self.df['Volume(Millions)'].iloc[index]
+        return self.df['Volume'].iloc[index]
 
     def get_ma_diff(self, index):
         return self.df['FastSMA'].iloc[index] - self.df['SlowSMA'].iloc[index]
