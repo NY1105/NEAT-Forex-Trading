fbfa9f5bd58261fc74d3b1faddc554b78597cf84
NY1105 <yanwaiyin1105@gmail.com>
2022-07-21 10:07:46 +0800

    upload AUDCAD

--- getdata.py
+++ getdata.py
@@ -3,16 +3,17 @@ from fetch import *
 from utils import daysinwhichmonth
 
 SYMBOLS = [
-    'AUDCAD', 'AUDCHF', 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADCHF', 'CADJPY',
-    'CHFJPY', 'EURAUD', 'EURCAD', 'EURCHF', 'EURGBP', 'EURJPY', 'EURNZD',
-    # 'EURUSD', 
-    'GBPAUD', 'GBPCAD', 'GBPCHF', 'GBPJPY', 'GBPNZD', 'GBPUSD',
-    'NZDCAD', 'NZDCHF', 'NZDJPY', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY'
+    # 'AUDCAD', 
+    'AUDCHF'
+    # , 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADCHF', 'CADJPY',
+    # 'CHFJPY', 'EURAUD', 'EURCAD', 'EURCHF', 'EURGBP', 'EURJPY', 'EURNZD',
+    # 'EURUSD', 'GBPAUD', 'GBPCAD', 'GBPCHF', 'GBPJPY', 'GBPNZD', 'GBPUSD',
+    # 'NZDCAD', 'NZDCHF', 'NZDJPY', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY'
 ]
 PRICE_TYPES = ['BID']
 
 
-for k in range(2010, 2012):  # year 2010-2022
+for k in range(2010, 2023):  # year 2010-2022
     for i in range(1, 13):  # month
         end_day = daysinwhichmonth(i, k)
         fetch(SYMBOLS, date(k, i, 1), date(k, i, end_day))
